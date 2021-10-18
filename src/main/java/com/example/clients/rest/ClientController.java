33db89afa448d788be693bccc065b54f3e408db9
Marcelo Zaguette <Marcelo.Zaguette@fit-tecnologia.org.br>
2021-10-18 17:02:05 -0300

    Allow CORS to Angular connection

--- src/main/java/com/example/clients/rest/ClientController.java
+++ src/main/java/com/example/clients/rest/ClientController.java
@@ -9,6 +9,7 @@ import com.example.clients.model.repository.ClientRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/clients")
+@CrossOrigin("*")
 public class ClientController {
     @Autowired
     private ClientRepository _repository;
